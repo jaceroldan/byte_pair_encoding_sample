f270644f617b3c192491bfa1052cc4e757a70063
Jace Roldan <jaceroldan16@gmail.com>
2024-08-31 13:41:28 +0800

    Add unique character extraction from the words

--- bpe.py
+++ bpe.py
@@ -1,4 +1,3 @@
-import random
 import json
 import pandas as pd
 import os
@@ -7,17 +6,32 @@ import os
 
 df = None
 
+def extract_unique_chars(s):
+    count_dict = {}
+    for c in s:
+        if type(count_dict.get(c, None)) == int:
+            count_dict[c] += 1
+        else:
+            count_dict[c] = 0
+
+    return count_dict
+
+
 if __name__ == '__main__':
     df = pd.read_csv('./datasets/train.csv')
     
     items = df.sample(n=1000)
-    print(items.shape)
-    print(items.head(n=10))
+
     n = 0
+    samples = []
     while n < 1000:
         curr_path = os.path.join(os.getcwd(), 'datasets', 'train', items.iloc[n]['Id'] + '.json')
 
         with open(curr_path, 'r') as file:
             curr_json = json.load(file)
-            print(curr_json)
+            # TODO: Include the cj['section_title'] in the parsing?
+            samples.append(''.join([cj['text'] for cj in curr_json]))
         n += 1
+
+    print(extract_unique_chars(samples[0]))
+    
