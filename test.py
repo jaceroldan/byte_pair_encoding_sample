858464becf8b3f2e6ce67c67b3b38af7ee6ad519
Jace Roldan <jaceroldan16@gmail.com>
2024-09-04 12:12:06 +0800

    Use set comparison in retrieval of matching statistics
    * Total BPE Tokens
    * Total Wordpiece Tokens
    * Tokens only in BPE
    * Tokens only in WordPiece
    * Intersection Percentage

--- test.py
+++ test.py
@@ -12,51 +12,60 @@ class TestRunner:
         df = None
         df = pd.read_csv('./datasets/train.csv')
         
-        items = df.sample(n=1000, random_state=42)
+        n = 10
+        items = df.sample(n=n, random_state=42)
 
-        n = 0
+        i = 0
         self.samples = []
-        while n < 1000:
+        while i < n:
             curr_path = os.path.join(
-                os.getcwd(), 'datasets', 'train', items.iloc[n]['Id'] + '.json')
+                os.getcwd(), 'datasets', 'train', items.iloc[i]['Id'] + '.json')
 
             with open(curr_path, 'r') as file:
                 curr_json = json.load(file)
                 self.samples.append(''.join([cj['text'] for cj in curr_json]))
-            n += 1
+            i += 1
     
     def run_results(self):
         mt_bpe = MultiThreadedBytePairEncoder(samples=self.samples, k=200)
-        bpe_tokens = mt_bpe.run()
+        bpe_tokens_list = mt_bpe.run()
 
         mt_wordpiece = MultiThreadedBertTokenizer(samples=self.samples)
-        wordpiece_tokens = mt_wordpiece.run()
+        wordpiece_tokens_list = mt_wordpiece.run()
 
-        only_in_bpe = bpe_tokens - wordpiece_tokens
+        overall_report = []
 
-        # Tokens unique to WordPiece
-        only_in_wordpiece = wordpiece_tokens - bpe_tokens
+        for i in range(len(self.samples)):
+            bpe_tokens = set(bpe_tokens_list[i])
+            wordpiece_tokens = set(wordpiece_tokens_list[i])
 
-        # Intersection of tokens (common tokens)
-        intersection = bpe_tokens & wordpiece_tokens
+            # Tokens unique to BPE
+            only_in_bpe = bpe_tokens - wordpiece_tokens
 
-        # Union of all tokens
-        union = bpe_tokens | wordpiece_tokens
+            # Tokens unique to WordPiece
+            only_in_wordpiece = wordpiece_tokens - bpe_tokens
 
-        # Percentage of intersection over union
-        intersection_percentage = len(intersection) / len(union) * 100
+            # Intersection of tokens (common tokens)
+            intersection = bpe_tokens & wordpiece_tokens
 
-        # Generate report
-        report = {
-            "Total BPE Tokens": len(bpe_tokens),
-            "Total WordPiece Tokens": len(wordpiece_tokens),
-            "Tokens only in BPE": list(only_in_bpe),
-            "Tokens only in WordPiece": list(only_in_wordpiece),
-            "Intersection Percentage": f"{intersection_percentage:.2f}%"
-        }
+            # Union of all tokens
+            union = bpe_tokens | wordpiece_tokens
 
-        # Display the report
-        pprint.pprint(report)
+            # Percentage of intersection over union
+            intersection_percentage = len(intersection) / len(union) * 100
+
+            # Generate report
+            report = {
+                "Total BPE Tokens": len(bpe_tokens),
+                "Total WordPiece Tokens": len(wordpiece_tokens),
+                "Tokens only in BPE": list(only_in_bpe),
+                "Tokens only in WordPiece": list(only_in_wordpiece),
+                "Intersection Percentage": f"{intersection_percentage:.2f}%"
+            }
+            overall_report.append(report)
+
+            # Display the report
+            pprint.pprint(report)
 
 
 if __name__ == '__main__':
